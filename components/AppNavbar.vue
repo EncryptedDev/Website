e3ef838468a3253e4f9e1c81d1022bee52775e37
Rhys <encryptedd.dev@gmail.com>
2021-03-22 22:42:05 +0000

    🆕 New Page

--- components/AppNavbar.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div>
-    <ul class="text-center w-auto">
-      <li class="inline font-bold">home</li>
-      <li class="inline">projects</li>
-      <li class="inline">contact</li>
-    </ul>
-  </div>
-</template>
