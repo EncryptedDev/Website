d0065c7ee34d1190fb117b01ba0d487f12ae3284
Rhys <encryptedd.dev@gmail.com>
2021-03-27 11:31:22 +0000

    ✨ Simplified.

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,6 @@
 export default {
-  // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: "Website",
+    title: "EncryptedDev",
     htmlAttrs: {
       lang: "en",
     },
@@ -12,22 +11,7 @@ export default {
     ],
     link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
   },
-
-  // Global CSS: https://go.nuxtjs.dev/config-css
   css: ["~/assets/css/style.css", "~/assets/css/animation.css"],
-
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
-
-  // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
-
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: ["@nuxtjs/tailwindcss"],
-
-  // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [],
-
-  // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {},
 };
