eb470320d2fe22c8fba91efdd66b6e5a48b1d843
Rhys <encryptedd.dev@gmail.com>
2021-03-27 22:51:04 +0000

    🆕 Added FontAwesome.

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,17 +1,22 @@
 export default {
   head: {
+    script: [
+      {
+        src: "https://kit.fontawesome.com/e904827701.js"
+      }
+    ],
     title: "EncryptedDev",
     htmlAttrs: {
-      lang: "en",
+      lang: "en"
     },
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
-      { hid: "description", name: "description", content: "" },
+      { hid: "description", name: "description", content: "" }
     ],
-    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
   },
   css: ["~/assets/css/style.css"],
   components: true,
-  buildModules: ["@nuxtjs/tailwindcss", "@nuxtjs/color-mode"],
+  buildModules: ["@nuxtjs/tailwindcss", "@nuxtjs/color-mode"]
 };
