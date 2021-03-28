c33988d84ad53dee06b900d2f883d760c183e2e3
Rhys <encryptedd.dev@gmail.com>
2021-03-28 23:06:05 +0100

    ✨ New Header.

---
+++ components/About.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="flex h-3/4">
+    <div class="m-auto w-4/5">
+      <h1 class="text-4xl text-center heading">
+        About <span class="text-purple-400"> me</span>
+      </h1>
+      <div class="flex flex-row gap-4 mt-5">
+        <div
+          class="bg-darkGrey rounded-3xl mt-3 mb-5 w-1/2 py-5 px-20 content text-center ml-auto mr-auto"
+        >
+          <p class="text-2xl heading text-center text-purple-400">
+            Introduction
+          </p>
+          <p class="text-xl">
+            I'm Encrypted, formally known as Rhys. An aspiring full-stack
+            developer from England.
+          </p>
+        </div>
+
+        <div
+          class="bg-darkGrey rounded-3xl mt-3 mb-5 w-1/2 py-5 px-20 content text-center ml-auto mr-auto"
+        >
+          <p class="text-2xl heading text-center text-purple-400">Setup</p>
+          <p class="content text-lg">
+            <i class="fab fa-apple"></i> Macbook Pro M1 (2021)
+          </p>
+          <p class="content text-lg">
+            <i class="fas fa-microphone"></i> RODE NT-USB (2021)
+          </p>
+          <p class="content text-lg">
+            <i class="fas fa-headphones"></i> SteelSeries Arctis 7 (2019)
+          </p>
+        </div>
+      </div>
+      <p class="text-2xl text-center heading">
+        Connect with <span class="text-purple-400">me</span>
+      </p>
+
+      <div class="flex flex-row gap-4">
+        <div
+          class="bg-darkGrey rounded-3xl mt-3 mb-5 w-1/6 py-3 px-10 content text-center ml-auto hover:bg-lightGrey"
+        >
+          <a href="https://twitter.com/Encrypted_UK" class="text-center"
+            ><i class="fab fa-twitter"></i> Twitter</a
+          >
+        </div>
+        <div
+          class="bg-darkGrey rounded-3xl mt-3 mb-5 w-1/6 py-3 px-10 content text-center mr-auto hover:bg-lightGrey"
+        >
+          <a href="https://github.com/EncryptedDev"
+            ><i class="fab fa-github"></i> GitHub</a
+          >
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
