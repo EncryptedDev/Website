191ba9d8a80644eee1566cbeb0d13bd0f2ce150c
Rhys <encryptedd.dev@gmail.com>
2021-03-28 23:05:35 +0100

    🎨 Custom Styling.

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,22 +8,15 @@ module.exports = {
   ],
   theme: {
     darkSelector: ".dark-mode",
-    // colors: {
-    //   bgdark: "16171b",
-    //   textHeaderDark: "f5f5f5",
-    //   textBodyDark: "f8f8f8",
-    //   textSubHeaderDark: "f2f2f2",
-
-    //   bglight: "f5f5f5",
-    //   textHeaderLight: "1c1c1c",
-    //   textBodyDark: "2c2c2c",
-    //   textSubHeaderDark: "1d1d1d",
-    // },
-  },
-  variants: {
-    // backgroundColor: ["dark", "dark-hover", "dark-group-hover", "dark-even", "dark-odd", "hover", "responsive"],
-    // borderColor: ["dark", "dark-focus", "dark-focus-within", "hover", "responsive"],
-    // textColor: ["dark", "dark-hover", "dark-active", "hover", "responsive"]
+    extend: {
+      fontFamily: {
+        custom: ["BasierCircle"],
+      },
+      colors: {
+        darkGrey: "#191919",
+        lightGrey: "#1c1c1c",
+      },
+    },
   },
   plugins: [require("tailwindcss-dark-mode")()],
 };
