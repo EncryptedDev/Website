9ae23d9e5d18d82b635cb1cbd8bc9304d9504ae8
Rhys <encryptedd.dev@gmail.com>
2021-03-28 23:06:18 +0100

    ✨ Updates.

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,15 @@
 <template>
-  <div class="py-20 px-20 dark:bg-black dark:text-textHeaderDark">
-    <Nav />
+  <div class="backgroundDark text-white">
     <Header />
-    <Projects />
+    <About />
   </div>
 </template>
 
 <script>
 import Header from "../components/Header";
-import Projects from "../components/Projects";
-import Nav from "../components//Navbar";
+import About from "../components/About";
 
 export default {
-  components: { Header, Projects, Nav },
+  components: { About, Header },
 };
 </script>
