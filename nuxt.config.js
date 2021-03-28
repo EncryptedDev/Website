4577174226911ffc1251e9ba0ac21ff0a0083ce4
Rhys <encryptedd.dev@gmail.com>
2021-03-28 23:06:27 +0100

    🗑 Removed modules.

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,21 +2,21 @@ export default {
   head: {
     script: [
       {
-        src: "https://kit.fontawesome.com/e904827701.js"
-      }
+        src: "https://kit.fontawesome.com/e904827701.js",
+      },
     ],
     title: "EncryptedDev",
     htmlAttrs: {
-      lang: "en"
+      lang: "en",
     },
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
-      { hid: "description", name: "description", content: "" }
+      { hid: "description", name: "description", content: "" },
     ],
-    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/logo.png" }],
   },
   css: ["~/assets/css/style.css"],
   components: true,
-  buildModules: ["@nuxtjs/tailwindcss", "@nuxtjs/color-mode"]
+  buildModules: ["@nuxtjs/tailwindcss", "@nuxtjs/color-mode"],
 };
