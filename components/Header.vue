f994fec384204b10aa30410453673d577999dd93
Rhys <encryptedd.dev@gmail.com>
2021-03-29 22:39:01 +0100

    🎨 Text fixes.

--- components/Header.vue
+++ components/Header.vue
@@ -4,7 +4,7 @@
       <h1 class="text-4xl heading">
         <span class="text-purple-400">Hello!</span> I'm Encrypted
       </h1>
-      <p class="text-xl text-center text-lightGrey py-3 content">
+      <p class="text-xl text-center text-white py-3 content">
         an aspiring <span class="text-purple-400">full-stack developer</span>
       </p>
     </div>
