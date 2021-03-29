15bf9dc3cced23aa4aa00d777b6fedc952a27e57
Rhys <encryptedd.dev@gmail.com>
2021-03-29 22:38:32 +0100

    ✨ Simplified.

--- components/About.vue
+++ components/About.vue
@@ -4,54 +4,20 @@
       <h1 class="text-4xl text-center heading">
         About <span class="text-purple-400"> me</span>
       </h1>
-      <div class="flex flex-row gap-4 mt-5">
-        <div
-          class="bg-darkGrey rounded-3xl mt-3 mb-5 w-1/2 py-5 px-20 content text-center ml-auto mr-auto"
-        >
-          <p class="text-2xl heading text-center text-purple-400">
-            Introduction
-          </p>
-          <p class="text-xl">
-            I'm Encrypted, formally known as Rhys. An aspiring full-stack
-            developer from England.
-          </p>
-        </div>
-
-        <div
-          class="bg-darkGrey rounded-3xl mt-3 mb-5 w-1/2 py-5 px-20 content text-center ml-auto mr-auto"
-        >
-          <p class="text-2xl heading text-center text-purple-400">Setup</p>
-          <p class="content text-lg">
-            <i class="fab fa-apple"></i> Macbook Pro M1 (2021)
-          </p>
-          <p class="content text-lg">
-            <i class="fas fa-microphone"></i> RODE NT-USB (2021)
-          </p>
-          <p class="content text-lg">
-            <i class="fas fa-headphones"></i> SteelSeries Arctis 7 (2019)
-          </p>
-        </div>
-      </div>
-      <p class="text-2xl text-center heading">
-        Connect with <span class="text-purple-400">me</span>
-      </p>
-
-      <div class="flex flex-row gap-4">
-        <div
-          class="bg-darkGrey rounded-3xl mt-3 mb-5 w-1/6 py-3 px-10 content text-center ml-auto hover:bg-lightGrey"
-        >
-          <a href="https://twitter.com/Encrypted_UK" class="text-center"
-            ><i class="fab fa-twitter"></i> Twitter</a
-          >
-        </div>
-        <div
-          class="bg-darkGrey rounded-3xl mt-3 mb-5 w-1/6 py-3 px-10 content text-center mr-auto hover:bg-lightGrey"
-        >
-          <a href="https://github.com/EncryptedDev"
-            ><i class="fab fa-github"></i> GitHub</a
-          >
-        </div>
-      </div>
+      <Introduction />
+      <div class="lg:flex lg:flex-row lg:gap-4 mt-5"><Setup /><Setup /></div>
+      <Socials />
     </div>
   </div>
 </template>
+
+<script>
+import DiscordModal from "../components/About/DiscordModal";
+import Introduction from "../components/About/Introduction";
+import Setup from "../components/About/Setup";
+import Socials from "../components/About/Socials";
+
+export default {
+  components: { DiscordModal, Introduction, Setup, Socials },
+};
+</script>
